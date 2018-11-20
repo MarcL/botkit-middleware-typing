d34ab58012b1cf87549792378be0e6410238d888
Marc Littlemore <marc.littlemore@bbc.co.uk>
2018-11-20 22:23:05 +0000

    Update README + formatting

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # botkit-middleware-typing
+
 BotKit middleware to enable typing indicator on platforms that support it
+
+## WIP
+
+Don't use this yet!
--- src/index.js
+++ src/index.js
@@ -3,49 +3,49 @@ const AVERAGE_CHARACTERS_PER_MINUTE = AVERAGE_WORDS_PER_MINUTE * 7;
 const DEFAULT_ATTACHMENT_TEXT_LENGTH = 80;
 
 const calculateTypingDelay = response => {
-  let textLength;
+    let textLength;
 
-  if (typeof response === 'string') {
-    textLength = response.length;
-  } else if (response.text) {
-    textLength = response.text.length;
-  } else {
-    textLength = DEFAULT_ATTACHMENT_TEXT_LENGTH;
-  }
+    if (typeof response === 'string') {
+        textLength = response.length;
+    } else if (response.text) {
+        textLength = response.text.length;
+    } else {
+        textLength = DEFAULT_ATTACHMENT_TEXT_LENGTH;
+    }
 
-  return Math.min(
-    Math.floor(textLength / (AVERAGE_CHARACTERS_PER_MINUTE / 60)) * 1000,
-    5000
-  );
+    return Math.min(
+        Math.floor(textLength / (AVERAGE_CHARACTERS_PER_MINUTE / 60)) * 1000,
+        5000
+    );
 };
 
 const typingIndicatorMessage = (bot, message) => ({
-  recipient: { id: message.to },
-  channel: message.channel,
-  sender_action: 'typing_on'
+    recipient: { id: message.to },
+    channel: message.channel,
+    sender_action: 'typing_on',
 });
 const botSupportsTyping = bot => bot.startTyping;
 
 const isTypingMessage = message =>
-  message.sender_action && message.sender_action === 'typing_on';
+    message.sender_action && message.sender_action === 'typing_on';
 
 const botkitMiddlewareTyping = (bot, message, next) => {
-  const send = () => {
-    console.log('Bot typing middleware');
-    if (botSupportsTyping(bot) && !isTypingMessage(message)) {
-      const typingIndicator = typingIndicatorMessage(bot, message);
-      const typingDelay = calculateTypingDelay(message);
-      bot.send(typingIndicator, () => {
-        setTimeout(() => next(), typingDelay);
-      });
-    } else {
-      next();
-    }
-  };
+    const send = () => {
+        console.log('Bot typing middleware');
+        if (botSupportsTyping(bot) && !isTypingMessage(message)) {
+            const typingIndicator = typingIndicatorMessage(bot, message);
+            const typingDelay = calculateTypingDelay(message);
+            bot.send(typingIndicator, () => {
+                setTimeout(() => next(), typingDelay);
+            });
+        } else {
+            next();
+        }
+    };
 
-  return {
-    send
-  };
+    return {
+        send,
+    };
 };
 
 module.exports = botkitMiddlewareTyping;
