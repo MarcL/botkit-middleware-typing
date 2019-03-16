a640176d97a175d8b97d1f17e3a58f0292f6220e
Marc Littlemore <marc.littlemore@bbc.co.uk>
2019-03-16 08:54:35 +0000

    test: initial tests for typing indicator

--- src/index.js
+++ src/index.js
@@ -19,33 +19,77 @@ const calculateTypingDelay = response => {
     );
 };
 
-const typingIndicatorMessage = (bot, message) => ({
+const typingIndicatorMessage = message => ({
     recipient: { id: message.to },
     channel: message.channel,
     sender_action: 'typing_on',
 });
+
 const botSupportsTyping = bot => bot.startTyping;
 
+// Currently only supports Facebook response
 const isTypingMessage = message =>
     message.sender_action && message.sender_action === 'typing_on';
 
-const botkitMiddlewareTyping = (bot, message, next) => {
-    const send = () => {
-        console.log('Bot typing middleware');
-        if (botSupportsTyping(bot) && !isTypingMessage(message)) {
-            const typingIndicator = typingIndicatorMessage(bot, message);
-            const typingDelay = calculateTypingDelay(message);
-            bot.send(typingIndicator, () => {
-                setTimeout(() => next(), typingDelay);
-            });
-        } else {
-            next();
-        }
-    };
-
-    return {
-        send,
-    };
+const botkitMiddlewareTyping = (config = {}) => (bot, message, next) => {
+    if (botSupportsTyping(bot) && !isTypingMessage(message)) {
+        //         const typingIndicator = typingIndicatorMessage(message);
+        //         const typingDelay = calculateTypingDelay(message);
+        bot.send(typingIndicatorMessage(message), () => {
+            setTimeout(() => next(), typingDelay);
+        });
+    } else {
+        next();
+    }
 };
+
+// const config = require('../config');
+
+// const AVERAGE_WORDS_PER_MINUTE = 85;
+// const AVERAGE_CHARACTERS_PER_MINUTE = AVERAGE_WORDS_PER_MINUTE * 7;
+// const DEFAULT_ATTACHMENT_TEXT_LENGTH = 80;
+
+// const MAXIMUM_TYPING_DELAY =
+//     config.get('application.maximumTypingDelaySeconds') * 1000;
+
+// const calculateTypingDelay = response => {
+//     let textLength;
+//     if (typeof response === 'string') {
+//         textLength = response.length;
+//     } else if (response.text) {
+//         textLength = response.text.length;
+//     } else {
+//         textLength = DEFAULT_ATTACHMENT_TEXT_LENGTH;
+//     }
+
+//     const textSpeed =
+//         Math.floor(textLength / (AVERAGE_CHARACTERS_PER_MINUTE / 60)) * 1000;
+
+//     return Math.min(textSpeed, MAXIMUM_TYPING_DELAY);
+// };
+
+// const typingIndicatorMessage = message => ({
+//     recipient: { id: message.to },
+//     channel: message.channel,
+//     sender_action: 'typing_on',
+// });
+
+// const botSupportsTyping = bot => bot.startTyping;
+
+// const isTypingMessage = message =>
+//     message.sender_action && message.sender_action === 'typing_on';
+
+// const typingMiddleware = (bot, message, next) => {
+//     if (botSupportsTyping(bot) && !isTypingMessage(message)) {
+//         const typingIndicator = typingIndicatorMessage(message);
+//         const typingDelay = calculateTypingDelay(message);
+
+//         bot.send(typingIndicator, () => {
+//             setTimeout(() => next(), typingDelay);
+//         });
+//     } else {
+//         next();
+//     }
+// };
 
 module.exports = botkitMiddlewareTyping;
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,5 +1,88 @@
-// const bokitMiddlewareTyping = require('../../src/index');
+const typingMiddleware = require('../../src/index');
 
-test('testing Jest', () => {
-    expect(3).toBe(3);
+test('should return a function', () => {
+    expect(typeof typingMiddleware).toBe('function');
+});
+
+describe('when typing is not supported by the bot platform', () => {
+    let spyNext;
+    const fakeBot = {
+        startTyping: false,
+    };
+
+    beforeEach(() => {
+        spyNext = jest.fn();
+    });
+
+    test('should call next', () => {
+        const fakeMessage = {};
+        const sendMiddleware = typingMiddleware();
+
+        sendMiddleware(fakeBot, fakeMessage, spyNext);
+
+        expect(spyNext.mock.calls.length).toBe(1);
+    });
+});
+
+describe('when typing is supported by the bot platform', () => {
+    let spyNext;
+    let spyBotSend;
+    let fakeBot;
+
+    beforeEach(() => {
+        spyNext = jest.fn();
+        spyBotSend = jest.fn();
+
+        fakeBot = {
+            startTyping: true,
+            send: spyBotSend,
+        };
+    });
+
+    test('should call next if message contains typing indicator', () => {
+        const fakeMessage = {
+            sender_action: 'typing_on',
+        };
+
+        const sendMiddleware = typingMiddleware();
+
+        sendMiddleware(fakeBot, fakeMessage, spyNext);
+
+        expect(spyNext).toHaveBeenCalledTimes(1);
+    });
+
+    test('should call bot.send', () => {
+        const fakeMessage = {};
+
+        const sendMiddleware = typingMiddleware();
+
+        sendMiddleware(fakeBot, fakeMessage, spyNext);
+
+        expect(spyBotSend).toHaveBeenCalledTimes(1);
+    });
+
+    test('should call bot.send with expected message', () => {
+        const expectedUserId = 'expectedUserId';
+        const expectedChannel = 'expectedChannel';
+
+        const fakeMessage = {
+            to: expectedUserId,
+            channel: expectedChannel,
+        };
+
+        const expectedMessage = {
+            recipient: { id: expectedUserId },
+            channel: expectedChannel,
+            sender_action: 'typing_on',
+        };
+
+        const sendMiddleware = typingMiddleware();
+
+        sendMiddleware(fakeBot, fakeMessage, spyNext);
+
+        expect(spyBotSend).toHaveBeenCalledWith(
+            expectedMessage,
+            expect.any(Function)
+        );
+    });
 });
